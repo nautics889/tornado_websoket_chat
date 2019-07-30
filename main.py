c916541c8c27f8d8af2c16d0ca579361b43277a8
nautics889 <cyberukr@gmail.com>
2019-07-30 23:15:10 +0300

    Implemented: timestamp for messages

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ import logging
 import motor
 from datetime import datetime
 import json
-from bson import json_util
 
 
 logging.basicConfig(format='%(asctime)s  %(levelname)-8s '
                            '[%(filename)s:%(lineno)d] %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S')
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -34,21 +33,25 @@ class ChatHandler(tornado.websocket.WebSocketHandler):
         client = motor.motor_tornado.MotorClient()
         db = client.test
         async for m in db.foobar.find({}, limit=5).sort([('_id', -1)]):
-            m.pop('_id')
-            #m['timestamp'] = str(m['timestamp'])
-            self.write_message(json.dumps(m, default=str))
+            data = {'content': m.pop('content'),
+                    'timestamp': str(m.pop('timestamp'))}
+            logging.info(f'MESSAGE DATA: {data}')
+            await self.write_message(json.dumps(data))
 
-    async def on_message(self, message):
-        logging.info(f'ON MESSAGE METHOD, {message}')
+    async def on_message(self, content):
+        logging.info(f'ON MESSAGE METHOD, {content}')
+
+        received_at = datetime.now()
+
         client = motor.motor_tornado.MotorClient()
         db = client.test
-
-        res = await db.foobar.insert_one({'message': message,
-                                          'timestamp': datetime.now()})
+        res = await db.foobar.insert_one({'content': content,
+                                          'timestamp': received_at})
         logging.debug(res)
 
         for connection in self.connections:
-            connection.write_message(message)
+            connection.write_message({'content': content,
+                                      'timestamp': str(received_at)})
 
     def close(self):
         logging.info('Someone has left the chat...')
