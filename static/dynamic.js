db407d015a023a305f8e77d1fa1e884f8b0eef65
nautics889 <cyberukr@gmail.com>
2019-08-21 20:08:26 +0300

    Implemented: FE messages ouput div

--- static/dynamic.js
+++ static/dynamic.js
@@ -6,24 +6,48 @@ $("#connect_btn").on("click", function() {
         console.log(ws_connection);
 
         $("#connect_btn").html('Disconnect');
-        $("#output").append(`<input type="text" id="message_to_send">`)
-        $("#output").append(`<button id="send_btn">Send</button>`)
+        $("#output").append(`<input type="text" id="message_to_send">`);
+        $("#output").append(`<button id="send_btn">Send</button>`);
         $("#send_btn").on("click", function() {
             ws_connection.send(JSON.stringify({
                 content: $("#message_to_send").val()
             }));
-        })
+        });
 
         ws_connection.onmessage = function(event) {
-            $("#output").append(`<p>${event.data}</p>`)
+            msg = $(`<p class="msg">${event.data}</p>`).appendTo("#output");
+            msg.animate(
+                {
+                    right: '150px',
+                    opacity: 1
+                },
+                800
+            );
+
+            number_of_messages = $(".msg").length
+            if (number_of_messages > 5) {
+                $(".msg").slice(0, number_of_messages - 5).animate(
+                    {
+                        left: '150px',
+                        opacity: 0
+                    },
+                    800, function() {
+                        $(this).remove();
+                    }
+                );
+                $(".msg").slice(number_of_messages - 5).animate(
+                    {
+                        bottom: $(".msg").outerHeight(true),
+                    },
+                    800
+                );
+            };
         };
     }
     else if (ws_connection instanceof WebSocket) {
         if(ws_connection.readyState == 1) {
             ws_connection.close();
-            console.log(ws_connection);
             $("#connect_btn").html('Disconnect');
         }
     };
 });
-
